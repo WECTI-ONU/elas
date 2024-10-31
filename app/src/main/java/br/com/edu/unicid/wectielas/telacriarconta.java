9bece8d2a197258b5961db7054b58e53083af088
GilbertoAkira <akirakobayashi@hotmail.com>
2024-10-30 21:03:11 -0300

    Deletei uma tela redundante e adicionei a função de quando o botão switch estiver verdadeiro aparecerá um editText para colocar o nome social.

--- app/src/main/java/br/com/edu/unicid/wectielas/telacriarconta.java
+++ app/src/main/java/br/com/edu/unicid/wectielas/telacriarconta.java
@@ -4,6 +4,8 @@ import android.content.Intent; // Importa a classe Intent
 import android.os.Bundle;
 import android.view.View; // Importa a classe View
 import android.widget.Button; // Importa a classe Button
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch; // Importa a classe Switch
 
 import androidx.activity.EdgeToEdge;
@@ -20,14 +22,31 @@ public class telacriarconta extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_telacriarconta);
 
-        // Configura o listener para o Switch
-        Switch switchNomeSocial = findViewById(R.id.switchNomeSocial); // Acesse o Switch
-        switchNomeSocial.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                // Inicia a nova Activity quando o Switch for ativado
-                Intent intent = new Intent(telacriarconta.this, telacriarconta2.class);
-                startActivity(intent); // Inicie a nova Activity
-                switchNomeSocial.setChecked(false);
+        EditText edtNome1 = findViewById(R.id.edtNome1);
+        EditText edtCpf1 = findViewById(R.id.edtCpf1);
+        EditText edtDataDeNascimento1 = findViewById(R.id.edtDataDeNascimento1);
+        EditText edtCep1 = findViewById(R.id.edtCep1);
+        EditText edtRua1 = findViewById(R.id.edtRua1);
+        EditText edtComplemento1 = findViewById(R.id.edtComplemento1);
+        EditText edtCidade1 = findViewById(R.id.edtCidade1);
+        EditText edtNumero1 = findViewById(R.id.edtNumero1);
+        EditText edtBairro1 = findViewById(R.id.edtBairro1);
+        EditText edtEstado1 = findViewById(R.id.edtEstado1);
+        EditText edtEmail1 = findViewById(R.id.edtEmail1);
+        EditText edtSenha1 = findViewById(R.id.edtSenha1);
+
+        Switch switchNomeSocial = findViewById(R.id.switchNomeSocial);
+        EditText myEditText = findViewById(R.id.edtNomeSocial);
+        myEditText.setVisibility(View.GONE);
+
+        switchNomeSocial.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    myEditText.setVisibility(View.VISIBLE);
+                } else {
+                    myEditText.setVisibility(View.GONE);
+                }
             }
         });
 
