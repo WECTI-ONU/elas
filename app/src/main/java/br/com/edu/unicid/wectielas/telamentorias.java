f215004c05af011fb1ce3d649f84e6fb7c4c4080
João Pedro <Fagundesjoao159@hotmail.com>
2024-11-01 12:49:14 -0300

    01/10/24 Adicionei as Mentorias na tela Mentorias
    Ps - Não Consegui arrumar o botão de voltar!

--- app/src/main/java/br/com/edu/unicid/wectielas/telamentorias.java
+++ app/src/main/java/br/com/edu/unicid/wectielas/telamentorias.java
@@ -1,5 +1,6 @@
 package br.com.edu.unicid.wectielas;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,9 +33,14 @@ public class telamentorias extends AppCompatActivity implements MentoriaAdapter.
 
         // Criar lista de mentorias
         mentorias = new ArrayList<>();
-        mentorias.add(new Mentoria("Dr. Ana Silva", "Cardiologia", "10/12/2023", "14:00"));
-        mentorias.add(new Mentoria("Dr. João Santos", "Pediatria", "11/12/2023", "15:30"));
-        mentorias.add(new Mentoria("Dr. Fernando Fortunato", "Pediatria", "11/12/2023", "15:30"));
+        mentorias.add(new Mentoria("Desvendando a Tecnologia: Carreiras e Oportunidades no Mundo Digital", "Dra. Ana Costa - Engenheira de Software e Consultora de Inovação", "Tecnologia e Inovação Digital", "11 NOV - 10Hrs ás 12Hrs"));
+        mentorias.add(new Mentoria("Do Sonho á Realidade: Criando e Gerindo Seu Próprio Negócio", "Dra. Luisa Almeida - Empreendedora e Coach de Negócios", "Empreendedorismo e Gestão de Negócios", "12 NOV - 15Hrs ás 17Hrs"));
+        mentorias.add(new Mentoria("Independência Financeira para Mulheres: Construindo o Futuro", "Dra. Carla Martins - Consultora Financeira","Finanças Pessoais e Planejamento Financeiro", "14NOV - 9Hrs às 11Hrs"));
+        mentorias.add(new Mentoria("Liderança e Protagonismo Feminino: Conquistando Seu Espaço", "Dra. Sofia Ribeiro - Executiva de Recursos Humanos","Liderança Feminina", "15NOV - 18Hrs às 20Hrs"));
+        mentorias.add(new Mentoria("Equilíbrio Emocional e Carreira: Como Gerenciar o Estresse", "Dra. Mariana Oliveira - Psicóloga","Especialidade: Saúde Mental e Bem-Estar", "16NOV - 14Hrs às 16Hrs"));
+        mentorias.add(new Mentoria("Marketing para Mulheres Empreendedoras: Conquistando Clientes Online", "Dra. Juliana Antunes - Especialista Em Markeing Digital"," Marketing Digital e Redes Sociais", "17NOV - 16Hrs às 18Hrs"));
+        mentorias.add(new Mentoria("Explorando o Mundo Acadêmico: Caminhos e Oportunidades", "Dra. Paula Mendes - Professora Universitaria","Carreira Acadêmica e Pesquisa", "19NOV - 10Hrs às 12Hrs"));
+
 
         // Configurar adapter
         adapter = new MentoriaAdapter(mentorias, this);
