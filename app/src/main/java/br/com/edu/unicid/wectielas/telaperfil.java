f8f4c524def0a2a0d14ac091430bd2ab2ca5c4cd
pedroHdSO <pedrohenrique_santos@live.com>
2024-11-01 00:46:57 -0300

    Adicionado ListView nos icons e nas escritas dos mesmos Precisa: Fazer com que o avatar de perfil padrão aparessa e seja substituido somente se a foto de perfil for selecionada
    ~Pedro

--- app/src/main/java/br/com/edu/unicid/wectielas/telaperfil.java
+++ app/src/main/java/br/com/edu/unicid/wectielas/telaperfil.java
@@ -11,15 +11,10 @@ import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class telaperfil extends AppCompatActivity {
     ImageView arrow;
     ImageView profileImageView;
-    /*variável que define um launcher (iniciador) de activity,
-     * usado para abrir outra atividade e processar o resultado quando a ação é concluída.*/
     private ActivityResultLauncher<Intent> imagePickerLauncher;
     Button uploadButton;
 
@@ -28,34 +23,26 @@ public class telaperfil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_telaperfil);
-        //Flecha que irei usar para fazer o botão de retorno
+
         arrow = findViewById(R.id.arrow);
         profileImageView = findViewById(R.id.userProfile);
         uploadButton = findViewById(R.id.uploadButton);
 
-        //Launcher = Iniciador de activity
-
-        //registerForActivityResult = método registra um lançador para iniciar a atividade e receber o resultado.
         imagePickerLauncher = registerForActivityResult(
-                //d efine que o tipo de ação que será realizada envolve iniciar uma nova atividade (neste caso, abrir a galeria).
                 new ActivityResultContracts.StartActivityForResult(),
                 result -> {
-                    //verificando o resultado e pegue a imagem selecionada
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-                        //extrai o URI da imagem selecionada, que é o "endereço" da imagem no dispositivo.
                         Uri selectedImage = result.getData().getData();
-                        //seta o endereço da imagem no imageView
-                        profileImageView.setImageURI(selectedImage);
+                        if (selectedImage != null) {
+                            profileImageView.setImageURI(selectedImage); // Atualiza com a nova imagem
+                        }
                     }
                 }
         );
 
-        // Defina a ação do botão de upload para abrir a galeria
         uploadButton.setOnClickListener(v -> {
-            //cria um Intent que especifica a ação de abrir a galeria de imagens (usando Intent.ACTION_PICK)
-            //e a localização do conteúdo, neste caso, as imagens armazenadas
             Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            imagePickerLauncher.launch(intent); // Abra a galeria
+            imagePickerLauncher.launch(intent);
         });
     }
 }
